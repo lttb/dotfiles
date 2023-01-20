94e2c62d7c522850771c2d922fb00b94300d59b5
lttb <kenzhaev.artur@gmail.com>
2023-01-20 14:20:26 +0000

    feat: support codespaces

---
+++ codespaces.install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -z "$USER" ]; then
+    USER=$(id -un)
+fi
+
+echo >&2 "====================================================================="
+echo >&2 " Setting up codespaces environment"
+echo >&2 ""
+echo >&2 " USER        $USER"
+echo >&2 " HOME        $HOME"
+echo >&2 "====================================================================="
+
+cd $HOME
+
+# Make passwordless sudo work
+export SUDO_ASKPASS=/bin/true
+
+# Install fzf
+FZF_VERSION=0.30.0
+curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC $HOME/bin
+
+# Install neovim
+NVIM_VERSION=0.8.2
+sudo apt-get install -y libfuse2
+curl -L -o $HOME/bin/nvim https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
+chmod a+x $HOME/bin/nvim
