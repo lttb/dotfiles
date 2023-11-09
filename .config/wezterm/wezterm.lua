7b3dfe188ab84daebc237f31a8a37b6093b362ba
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-11-09 21:42:28 +0000

    feat(wezterm): update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,34 +1,39 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("Fira Code", { weight = 450, stretch = "Normal", style = "Normal" }),
-	font_size = 14.0,
-	line_height = 1.2,
+  font = wezterm.font("Fira Code", { weight = 450, stretch = "Normal", style = "Normal" }),
+  font_size = 15.0,
+  line_height = 1.2,
 
-	window_close_confirmation = "NeverPrompt",
-	color_scheme = "OneHalfDark",
+  window_close_confirmation = "NeverPrompt",
+  color_scheme = "zenbones_dark",
 
-	window_decorations = "RESIZE",
-	window_padding = {
-		left = 10,
-		right = 10,
-		top = 10,
-		bottom = 0,
-	},
+  window_decorations = "RESIZE",
+  window_padding = {
+    left = 10,
+    right = 10,
+    top = 50,
+    bottom = 0,
+  },
 
-	enable_scroll_bar = true,
+  enable_scroll_bar = true,
+  enable_kitty_keyboard = true,
+  enable_csi_u_key_encoding = true,
+  send_composed_key_when_left_alt_is_pressed = true,
+  send_composed_key_when_right_alt_is_pressed = true,
 
-	tab_max_width = 40,
+  tab_max_width = 40,
 
-	alternate_buffer_wheel_scroll_speed = 100,
+  alternate_buffer_wheel_scroll_speed = 100,
 
-	initial_cols = 164,
-	initial_rows = 40,
+  initial_cols = 164,
+  initial_rows = 40,
 
-	tab_bar_at_bottom = true,
+  tab_bar_at_bottom = true,
 
-	inactive_pane_hsb = {
-		saturation = 0.9,
-		brightness = 0.8,
-	},
+  inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.8,
+  },
+
 }
