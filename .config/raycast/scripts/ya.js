16d3c8b8b153ed707ffb9ac6b96072dc561aff27
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-01-05 22:21:17 +0000

    feat(raycast): update yabai script

---
+++ .config/raycast/scripts/ya.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env bun
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title ya
+// @raycast.mode compact
+
+// Optional parameters:
+// @raycast.icon 🤖
+// @raycast.argument1 {"optional": true, "type": "dropdown", "placeholder": "app", "data": [{"title": "win", "value": "win"}, {"title": "app", "value": "app"}, {"title": "lock", "value": "lock"}, {"title": "unlock", "value": "unlock"}, {"title": "lock_all", "value": "lock_all"}, {"title": "unlock_all", "value": "unlock_all"}]}
+// @raycast.argument2 {"optional": true, "type": "dropdown", "placeholder": "current", "data": [{"title": "current", "value": "current"}, {"title": "centre", "value": "centre"}, {"title": "main", "value": "main"}, {"title": "left", "value": "left"}, {"title": "right", "value": "right"}]}
+
+// Documentation:
+// @raycast.author Artur Kenzhaev
+// @raycast.authorURL https://github.com/lttb
+
+import {run} from './utils'
+
+const [type = 'win', command = 'centre'] = process.argv.slice(2)
+
+await run(type, command)
