1cd2069a824648d529c316fa71151c703f4f1cc4
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-01-06 23:05:26 +0000

    feat(zsh): add dd alias for neovide

--- .config/zsh/utils/utils.zsh
+++ .config/zsh/utils/utils.zsh
@@ -6,6 +6,11 @@ alias d='\
     GIT_DIR=$HOME/.local/share/yadm/repo.git \
     GIT_WORK_TREE=$HOME \
     nvim -c ":cd ~"'
+alias dd='\
+    DOTFILES=1 \
+    GIT_DIR=$HOME/.local/share/yadm/repo.git \
+    GIT_WORK_TREE=$HOME \
+    nv -- -c ":cd ~"'
 
 alias cl="clear"
 
@@ -26,35 +31,34 @@ safe_source() {
   if [ -f "$1" ]; then . "$1"; fi
 }
 
-code() { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
+code() { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*; }
 
 gob() {
-    branch_search="$(\
-      git branch --sort=-committerdate --format="%(if)%(HEAD)%(then)*%(else) %(end) %(refname:short): [%(committerdate)] @%(authorname) %(subject)" \
-      | fzf \
-      | sed 's/:.\{1,\}//' \
-      | sed 's/^[* ]*//' \
-    )"
+  branch_search="$(
+    git branch --sort=-committerdate --format="%(if)%(HEAD)%(then)*%(else) %(end) %(refname:short): [%(committerdate)] @%(authorname) %(subject)" |
+      fzf |
+      sed 's/:.\{1,\}//' |
+      sed 's/^[* ]*//'
+  )"
 
-    git checkout $branch_search
+  git checkout $branch_search
 }
 
 yb() {
-    yabai -m query --windows |
-      jq ".[] | select(.app | test(\"$1\")).id" |
-      xargs -L1 sh -c "
+  yabai -m query --windows |
+    jq ".[] | select(.app | test(\"$1\")).id" |
+    xargs -L1 sh -c "
         yabai -m window \$0 --toggle float &&
         yabai -m window \$0 $2
       "
 }
 
 ybc() {
-    yabai -m query --windows |
-      jq ".[] | select(.app | test($(yabai -m query --windows --window | jq '.app'))).id" |
-      xargs -L1 sh -c "
+  yabai -m query --windows |
+    jq ".[] | select(.app | test($(yabai -m query --windows --window | jq '.app'))).id" |
+    xargs -L1 sh -c "
         yabai -m window \$0 --toggle float &&
         yabai -m window \$0 $1
       "
 }
 # }}}
-
